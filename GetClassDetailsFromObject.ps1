67d9cc83f7fd2fa897be2bc797d8827f1b675121
Sourav Mahato <95037368+somahato@users.noreply.github.com>
2023-09-27 23:12:31 +0530

    Create GetClassDetailsFromObject.ps1

---
+++ GetClassDetailsFromObject.ps1
@@ -0,0 +1,27 @@
+$Objss = Invoke-RestMethod -Uri 'http://localhost:1234/API/Computer/windows' -UseDefaultCredentials
+#$Objss | gm
+
+$objs = $Objss.displayname
+Foreach ($obj in $objs)
+{
+
+# getting object information
+Write-Host "Working on $obj" -ForegroundColor Yellow
+$uriBase = 'http://localhost/OperationsManager'
+$criteria = "DisplayName LIKE '%$obj%'"
+$scomHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$scomHeaders.Add('Content-Type','application/json; charset=utf-8')
+$bodyraw = "Windows"
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($bodyraw)
+$EncodedText = [Convert]::ToBase64String($Bytes)
+$jsonbody = $EncodedText | ConvertTo-Json
+$auth = Invoke-RestMethod -Method POST -Uri "$uriBase/authenticate" -Headers $scomHeaders -body $jsonbody -UseDefaultCredentials -SessionVariable websession
+$jsonbody = $criteria | ConvertTo-Json
+$csrfToken = $websession.Cookies.GetCookies($uriBase) | ? { $_.Name -eq 'SCOM-CSRF-TOKEN' }
+$scomHeaders.Add('SCOM-CSRF-TOKEN', [System.Web.HttpUtility]::UrlDecode($csrfToken.Value))
+$response = Invoke-WebRequest -Uri "$uriBase/data/scomObjects" -Headers $scomHeaders -Method Post -Body $jsonbody -ContentType "application/json" -UseDefaultCredentials -WebSession $websession
+$Object = ConvertFrom-Json -InputObject $response.Content
+$Classes = $Object.scopeDatas.Classname
+$Classes
+
+}
